28e578db3916a618a77a5e8e267bad28137b5811
MatusVit <matvi@mail.ru>
2019-10-08 16:42:03 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+
+  while (str.length > 0) {
+    
+    let result = false;
+    
+    for (const arr in bracketsConfig) {
+      let pos = str.indexOf(bracketsConfig[arr][0] + bracketsConfig[arr][1]);
+      
+      if (pos != -1) {
+        str = str.slice(0, pos) + str.slice (pos+2);
+        result = true;
+      };
+    }
+
+    if (result == false) return false;
+    
+  }
+  return true;
 }
